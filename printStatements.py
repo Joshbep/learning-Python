e6383f9ce9f30ae7535763c54d0695bcf1977571
jbep <joshuabemartin@gmail.com>
2023-08-30 16:34:00 -0400

    learning some errors in python

--- printStatements.py
+++ printStatements.py
@@ -1,5 +1,12 @@
 #print("print this")
 #print("Hello "+"Josh Pasour")
 
-print("How do you make a hot dog stand?")
-print("You take away its chair!")
+#print("How do you make a hot dog stand?")
+#print("You take away its chair!")
+
+#todays_date = "8/30/23"
+#print(todays_date)
+
+#Arithmetic
+product = 2 * 2
+remainder = 1398 % 11
