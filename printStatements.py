d0730d6a21208b94212c8b78346b9998ca1f1c0f
jbep <joshuabemartin@gmail.com>
2023-09-01 16:37:04 -0400

    going over integers and floats

--- printStatements.py
+++ printStatements.py
@@ -37,10 +37,18 @@
 
 
 # going over integers
-cucumbers = 1
+# cucumbers = 1
 
-price_per_cucumber = 3.25
+# price_per_cucumber = 3.25
 
-total_cost = cucumbers * price_per_cucumber
+# total_cost = cucumbers * price_per_cucumber
 
-print(total_cost)
+# print(total_cost)
+
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers/num_people
+print(whole_cucumbers_per_person)
+
+float_cucumbers_per_person = float(cucumbers)/num_people
+print(float_cucumbers_per_person)
