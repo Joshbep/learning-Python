aeff14d3a9440fd39b4c3bf3dd9c0b87c7b51edc
jbep <joshuabemartin@gmail.com>
2023-09-05 17:28:42 -0400

    covering index

--- strings.py
+++ strings.py
@@ -9,9 +9,30 @@ brian = "Hello life!"
 
 
 # Put your variables above this line
-caesar = "Graham"
-praline = "John"
-viking = "Teresa"
-print caesar
-print praline
-print viking
+# caesar = "Graham"
+# praline = "John"
+# viking = "Teresa"
+# print caesar
+# print praline
+# print viking
+
+# The string below is broken. Fix it using the escape backslash!
+
+# 'This isn\'t flying, this is falling with style!'
+
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
