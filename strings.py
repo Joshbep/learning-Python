00af9c1fcee07c7443e6b8cb3a294242f2f2ace8
jbep <joshuabemartin@gmail.com>
2023-09-05 17:42:10 -0400

    finfishe dup the string section:

--- strings.py
+++ strings.py
@@ -32,22 +32,67 @@ numbered 0 to 5, as shown below:
 So if you wanted "Y", you could just type
 "PYTHON"[1] (always start counting from 0!)
 """
-fifth_letter = "MONTY"[4]
+# fifth_letter = "MONTY"[4]
 
-print fifth_letter
+# print fifth_letter
 
-parrot = "Norwegian Blue"
-print len(parrot)
+# parrot = "Norwegian Blue"
+# print len(parrot)
 
-parrot = "Norwegian Blue".lower()
-print parrot
+# parrot = "Norwegian Blue".lower()
+# print parrot
 
-parrot = "norwegian blue".upper()
+# parrot = "norwegian blue".upper()
 
-print parrot
+# print parrot
 
-"""Declare and assign your variable on line 4,
-then call your method on line 5!"""
+# """Declare and assign your variable on line 4,
+# then call your method on line 5!"""
 
-pi = 3.14
-print str(pi)
+# pi = 3.14
+# print str(pi)
+
+# ministry = "The Ministry of Silly Walks"
+
+# print len(ministry)
+# print ministry.upper()
+
+# """Tell Python to print "Monty Python"
+# to the console on line 4!"""
+
+# print("Monty Python")
+
+# """Assign the string "Ping!" to
+# the variable the_machine_goes on
+# line 5, then print it out on line 6!"""
+
+
+# the_machine_goes = "Ping!"
+# print(the_machine_goes)
+
+# Print the concatenation of "Spam and eggs" on line 3!
+
+# print("Spam " + "and " + "eggs")
+
+# # Turn 3.14 into a string on line 3!
+
+# print "The value of pi is around " + str(3.14)
+
+# string_1 = "Camelot"
+# string_2 = "place"
+
+# print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+# name = "Alex"
+# quest = "Teaching Python"
+# color = "Blue"
+
+# print "Ah, so your name is %s, your quest is %s, " \
+# "and your favorite color is %s." % (name, quest, color)
+
+# pratice
+# Write your code below, starting on line 3!
+
+my_string = "hi"
+print len(my_string)
+print my_string.upper()
