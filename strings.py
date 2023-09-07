ce86b814ae393d0beafac3fa18d9b83664c7860d
jbep <joshuabemartin@gmail.com>
2023-09-07 17:01:16 -0400

    pretty time

--- strings.py
+++ strings.py
@@ -118,7 +118,13 @@ from datetime import datetime
 
 # print '%02d/%02d/%04d' % (now.month, now.day, now.year)
 
+# from datetime import datetime
+# now = datetime.now()
+
+# print '%02d:%02d:%02d' % (now.hour, now.minute, now.second)
+
 from datetime import datetime
 now = datetime.now()
 
-print '%02d:%02d:%02d' % (now.hour, now.minute, now.second)
+
+print '%02d/%02d/%04d %02d:%02d:%02d' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
