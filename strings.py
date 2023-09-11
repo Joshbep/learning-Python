a09c06f0e85d7b5625d228b9571ab0cc704c7f23
jbep <joshuabemartin@gmail.com>
2023-09-11 17:45:40 -0400

    conditionals

--- strings.py
+++ strings.py
@@ -123,8 +123,22 @@ from datetime import datetime
 
 # print '%02d:%02d:%02d' % (now.hour, now.minute, now.second)
 
-from datetime import datetime
-now = datetime.now()
+# from datetime import datetime
+# now = datetime.now()
 
 
 print '%02d/%02d/%04d %02d:%02d:%02d' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+
+def clinic():
+    print "You've just entered the clinic!"
+    print "Do you take the door on the left or the right?"
+    answer = raw_input("Type left or right and hit 'Enter'.").lower()
+    if answer == "left" or answer == "l":
+      print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+    elif answer == "right" or answer == "r":
+      print "Of course this is the Argument Room, I've told you that already!"
+    else:
+      print "You didn't pick left or right! Try again."
+      clinic()
+
+clinic()
