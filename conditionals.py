e50ec3484798e91238810129530d48282b7ea1b7
jbep <joshuabemartin@gmail.com>
2023-09-12 16:03:38 -0400

    conditionals

--- conditionals.py
+++ conditionals.py
@@ -15,16 +15,34 @@
 # Assign True or False as appropriate on the lines below!
 
 # Set this to True if 17 < 328 or to False if it is not.
-bool_one = True   # We did this one for you!
+# bool_one = True   # We did this one for you!
 
-# Set this to True if 100 == (2 * 50) or to False otherwise.
-bool_two = True
+# # Set this to True if 100 == (2 * 50) or to False otherwise.
+# bool_two = True
 
-# Set this to True if 19 <= 19 or to False if it is not.
-bool_three = True
+# # Set this to True if 19 <= 19 or to False if it is not.
+# bool_three = True
 
-# Set this to True if -22 >= -18 or to False if it is not.
-bool_four = False
+# # Set this to True if -22 >= -18 or to False if it is not.
+# bool_four = False
 
-# Set this to True if 99 != (98 + 1) or to False otherwise.
+# # Set this to True if 99 != (98 + 1) or to False otherwise.
+# bool_five = False
+
+# Assign True or False as appropriate on the lines below!
+
+# (20 - 10) > 15
+bool_one = False    # We did this one for you!
+
+# (10 + 17) == 3**16
+# Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
+bool_two = False
+
+# 1**2 <= -1
+bool_three = False
+
+# 40 * 4 >= -4
+bool_four = True
+
+# 100 != 10**2
 bool_five = False
