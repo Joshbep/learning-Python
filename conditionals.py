5cab3055fddf9d51b0d8e5e28d3138c012a06879
jbep <joshuabemartin@gmail.com>
2023-09-13 14:00:07 -0400

    booleans

--- conditionals.py
+++ conditionals.py
@@ -74,12 +74,22 @@ bool_one = False    # We did this one for you!
 
 # bool_five = True and True
 
-bool_one = 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
+# bool_one = 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
 
-bool_two = True or False
+# bool_two = True or False
 
-bool_three = 100**0.5 >= 50 or False
+# bool_three = 100**0.5 >= 50 or False
 
-bool_four = True or True
+# bool_four = True or True
 
-bool_five = 1**100 == 100**1 or 3 * 2 * 1 != 3 + 2 + 1
+# bool_five = 1**100 == 100**1 or 3 * 2 * 1 != 3 + 2 + 1
+
+bool_one = not True
+
+bool_two = not 3**4 < 4**3
+
+bool_three = not 10 % 3 <= 10 % 2
+
+bool_four = not 3**2 + 4**2 != 5**2
+
+bool_five = not not False
