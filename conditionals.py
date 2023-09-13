7040d2cecfafad2458e43f39253459e41ffb3cf5
jbep <joshuabemartin@gmail.com>
2023-09-13 14:11:52 -0400

    booleans

--- conditionals.py
+++ conditionals.py
@@ -94,12 +94,29 @@ bool_one = False    # We did this one for you!
 
 # bool_five = not not False
 
-bool_one = False or not True and True
+# bool_one = False or not True and True
 
-bool_two = False and not True or True
+# bool_two = False and not True or True
 
-bool_three = True and not (False or False)
+# bool_three = True and not (False or False)
 
-bool_four = not not True or False and not True
+# bool_four = not not True or False and not True
 
-bool_five = False or not (True and True)
+# bool_five = False or not (True and True)
+
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = True and True
+
+# Make me false!
+bool_three = False and True
+
+# Make me true!
+bool_four = not False
+
+# Make me true!
+bool_five = True or True
