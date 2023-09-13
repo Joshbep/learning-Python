4cdfd4aec456ca24890985a44002298f3698132a
jbep <joshuabemartin@gmail.com>
2023-09-13 14:32:42 -0400

    conditional statement

--- conditionals.py
+++ conditionals.py
@@ -155,14 +155,36 @@ bool_one = False    # We did this one for you!
 #     else:             
 #         return False
 
-def greater_less_equal_5(answer):
-    if answer > 5:
-        return 1
-    elif answer < 5:          
-        return -1
-    else:
-        return 0
+# def greater_less_equal_5(answer):
+#     if answer > 5:
+#         return 1
+#     elif answer < 5:          
+#         return -1
+#     else:
+#         return 0
         
-print greater_less_equal_5(4)
-print greater_less_equal_5(5)
-print greater_less_equal_5(6)
+# print greater_less_equal_5(4)
+# print greater_less_equal_5(5)
+# print greater_less_equal_5(6)
+
+# Complete the if and elif statements!
+def grade_converter(grade):
+    if grade >= 90:
+        return "A"
+    elif grade >= 80:
+        return "B"
+    elif grade >= 70:
+        return "C"
+    elif grade >= 65:
+        return "D"
+    else:
+        return "F"
+      
+# This should print an "A"      
+print grade_converter(92)
+
+# This should print a "C"
+print grade_converter(70)
+
+# This should print an "F"
+print grade_converter(61)
