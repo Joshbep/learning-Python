f9d3e3be45a208167e7d5e5814d3ab4eca8711ba
jbep <joshuabemartin@gmail.com>
2023-09-13 14:30:43 -0400

    conditional statement

--- conditionals.py
+++ conditionals.py
@@ -141,16 +141,28 @@ bool_one = False    # We did this one for you!
 # print using_control_once()
 # print using_control_again()
 
-answer = "'Tis but a scratch!"
+# answer = "'Tis but a scratch!"
 
-def black_knight():
-    if answer == "'Tis but a scratch!":
-        return True
-    else:             
-        return False
+# def black_knight():
+#     if answer == "'Tis but a scratch!":
+#         return True
+#     else:             
+#         return False
 
-def french_soldier():
-    if answer == "Go away, or I shall taunt you a second time!":
-        return True
-    else:             
-        return False
+# def french_soldier():
+#     if answer == "Go away, or I shall taunt you a second time!":
+#         return True
+#     else:             
+#         return False
+
+def greater_less_equal_5(answer):
+    if answer > 5:
+        return 1
+    elif answer < 5:          
+        return -1
+    else:
+        return 0
+        
+print greater_less_equal_5(4)
+print greater_less_equal_5(5)
+print greater_less_equal_5(6)
