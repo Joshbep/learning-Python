7888841ee0ad34cff875695ed7f45d663adf426d
jbep <joshuabemartin@gmail.com>
2023-10-02 18:58:51 -0400

    exercies

---
+++ ex.py
@@ -0,0 +1,67 @@
+# Exercise 1  
+# It is a good idea to read this book in front of a computer so you can try out the examples as you go.
+
+# Whenever you are experimenting with a new feature, you should try to make mistakes. For example, in the “Hello, world!” program, what happens if you leave out one of the quotation marks? What if you leave out both? What if you spell print wrong?
+# prints out error messages
+# print('hello')
+# This kind of experiment helps you remember what you read; it also helps when you are programming, because you get to know what the error messages mean. It is better to make mistakes now and on purpose than later and accidentally.
+
+# In a print statement, what happens if you leave out one of the parentheses, or both?
+# answer: SyntaxError: unexpected EOF while parsing
+# print('hello'
+# If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+# answer: SyntaxError: EOL while scanning string literal
+# print('hello')
+# You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
+# it prints 4
+# print(2++2)
+# In math notation, leading zeros are ok, as in 09. What happens if you try this in Python? What about 011?
+# answer:SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+# print(011)
+# What happens if you have two values with no operator between them?
+# it won't run
+# grow= 2 2
+# print(grow)
+# Exercise 2  
+# Start the Python interpreter and use it as a calculator.
+
+# How many seconds are there in 42 minutes 42 seconds?
+# seconds = 42 * 60
+# seconds += 42
+# print(seconds)
+# How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+# If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+
+# Buy this book at Amazon.com
+
+# Contribute
+# If you would like to make a contribution to support my books, you can use the button below and pay with PayPal. Thank you!
+# Pay what you want:
+
+# Small $1.00 USD
+# PayPal - The safer, easier way to pay online! 
+# Are you using one of our books in a class?
+# We'd like to know about it. Please consider filling out this short survey.
+
+# Think DSP
+
+
+
+# Think Java
+
+
+
+# Think Bayes
+
+
+
+# Think Python 2e
+
+
+
+# Think Stats 2e
+
+
+
+# Think Complexity
+
