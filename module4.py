8b0045f57c905f9225a9ab68be1c50654c5e77d5
jbep <joshuabemartin@gmail.com>
2023-10-20 18:00:33 -0400

    some exercises

---
+++ module4.py
@@ -0,0 +1,16 @@
+def concat(str1, str2):
+    return str1 + str2
+
+print(concat("go ","hi"))
+
+#Write a function that takes two string parameters and returns the string formed by concatenating those two strings together. Write a print statement that calls your function with the values "hello " and "world" and prints the function's return value.
+#Sample input: "hello ", "world"
+#Expected output: "hello world"
+
+def twice(num):
+    return num * 2
+
+#1. Write a function that takes a numeric parameter and returns twice that number. Write a print statement that calls your function with the number 5 and prints the function's return value.
+
+# Sample input: 5
+# Expected output: 10
